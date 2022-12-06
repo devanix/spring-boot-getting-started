f9a79e06d1e7b279634d378ca48a48060e0ef1f6
mjkim <mjkim@pentasecurity.com>
2022-12-07 02:38:42 +0900

    세션2. 자동 설정 이해 - @EnableAutoConfiguration없이 애플리케이션 실행

--- src/main/java/com/devanix/Application.java
+++ src/main/java/com/devanix/Application.java
@@ -1,11 +1,19 @@
 package com.devanix;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+//@EnableAutoConfiguration
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.setWebApplicationType(WebApplicationType.NONE);
+        springApplication.run(args);
     }
 }
