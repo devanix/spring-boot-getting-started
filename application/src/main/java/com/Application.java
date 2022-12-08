14074c16ddec3dbba8e6016a7958a3b8122a5b65
devanix <devanix@gmail.com>
2022-12-08 17:11:22 +0900

    세션2. 내장 웹 서버 응용: HTTPS 적용

--- application/src/main/java/com/Application.java
+++ application/src/main/java/com/Application.java
@@ -3,9 +3,17 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring";
+    }
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
