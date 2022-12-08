145b762ed0665d11070840d367f1a141a6027a21
devanix <devanix@gmail.com>
2022-12-08 17:43:22 +0900

    세션2. 내장 웹 서버 응용: undertow로 http2 활성화

--- application/src/main/java/com/Application.java
+++ application/src/main/java/com/Application.java
@@ -1,12 +1,7 @@
 package com;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,18 +14,18 @@ public class Application {
         return "Hello Spring";
     }
 
-    @Bean
-    public ServletWebServerFactory serverFactory() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
+//    @Bean
+//    public ServletWebServerFactory serverFactory() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//        return tomcat;
+//    }
 
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(8080);
-        return connector;
-    }
+//    private Connector createStandardConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(8080);
+//        return connector;
+//    }
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
