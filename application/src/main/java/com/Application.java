9453877010d75bd41c4173e701577f2e5e27887e
devanix <devanix@gmail.com>
2022-12-08 16:17:44 +0900

    세션2. 자동 설정 만들기 - Starter와 AotoConfigure
    Maven multi project 구조 변경

--- application/src/main/java/com/Application.java
+++ application/src/main/java/com/Application.java
@@ -1,19 +1,25 @@
-package com.devanix;
+package com;
 
+import com.devanix.Holoman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-//@SpringBootApplication
-@Configuration
-//@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
         SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(args);
+    }
+
+    @Bean
+    public Holoman holoman() {
+        Holoman holoman = new Holoman();
+        holoman.setHowLong(10);
+        holoman.setName("luka");
+        return holoman;
     }
 }
