b3c4f94e724f5fb31977f50b0f33cd96c70c3157
mjkim <mjkim@pentasecurity.com>
2022-12-27 06:40:59 +0900

    스프링 웹MVC 1부: 소개

--- application/src/main/java/com/devanix/Application.java
+++ application/src/main/java/com/devanix/Application.java
@@ -6,17 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello Spring";
-    }
 
     @Bean
     public ServletWebServerFactory serverFactory() {
