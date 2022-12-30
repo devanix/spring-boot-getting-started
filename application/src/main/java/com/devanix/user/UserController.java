51a88596c1f0fd5f024264ac6f7b6a4e3158b8e8
mjkim <mjkim@pentasecurity.com>
2022-12-30 18:57:23 +0900

    템플릿 엔진 Thymeleaf 적용 및 JUnit4 테스트

--- application/src/main/java/com/devanix/user/UserController.java
+++ application/src/main/java/com/devanix/user/UserController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "hello2";
     }
 
     @PostMapping("/users/create")
---
+++ application/src/test/java/com/devanix/controller/SampleControllerTest.java
@@ -0,0 +1,38 @@
+package com.devanix.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        /**
+         * 요청: "hello"
+         * 응답: 200
+         * 뷰 이름: "hello"
+         * 모델 이름&값: "name", "devanix"
+         */
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(view().name("hello"))
+                .andExpect(model().attribute("name", is("devanix")))
+                .andExpect(content().string(containsString("devanix")));
+    }
+}
